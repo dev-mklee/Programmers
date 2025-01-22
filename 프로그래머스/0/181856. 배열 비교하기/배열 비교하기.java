ff89737ecb4c4e15bb3a4bd9bc8d5ea909b1afb6
MK Lee <menabrea@naver.com>
2025-01-22 16:41:51 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length)
+            return 1;
+        else if (arr1.length < arr2.length)
+            return -1;
+        else {
+            int num1 = 0;
+            int num2 = 0;
+            for (int i = 0; i < arr1.length; i++) {
+                num1 += arr1[i];
+                num2 += arr2[i];
+            }
+            
+            return (num1 > num2) ? 1 : (num1 < num2) ? -1 : 0;
+        }
+    }
+}
